a0cb504b7cea45146ff4addda8860dc4266a04d7
ArielSanchez12 <riveraariel433@gmail.com>
2025-01-31 01:17:45 -0500

    apariencia admin gestion

--- src/PaqueteAdministrador/estadisticas.java
+++ src/PaqueteAdministrador/estadisticas.java
@@ -37,6 +37,7 @@ public class estadisticas extends conexion {
         menuFrame.setPreferredSize(new Dimension(1000, 600));
         menuFrame.setLocationRelativeTo(null);
         menuFrame.setIconImage(new ImageIcon("src/PaqueteRecursos/iconos/estadisticas.png").getImage());
+        menuFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         menuFrame.pack();
         menuFrame.setVisible(true);
 
